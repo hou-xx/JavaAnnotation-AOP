b432a0d0f5dc84388f4a6efffeb41bb745d075e9
houxx <hou_xiangxiang@126.com>
2018-08-30 19:29:43 +0800

    add annotation type

--- java-annotation-test/src/main/java/com/hxx/Test.java
+++ java-annotation-test/src/main/java/com/hxx/Test.java
@@ -1,5 +1,7 @@
 package com.hxx;
 
+import com.hxx.annotation.AnnotationDemo;
+import com.hxx.annotation.Author;
 import com.hxx.model.Person;
 
 import java.lang.annotation.Annotation;
@@ -17,14 +19,14 @@ public class Test {
     public static void main(String[] args) {
         Person person = new Person();
         Class<? extends Person> personClass = person.getClass();
-        // 打印 Class 的注解信息
+        // 打印 Class 上的注解信息
         Annotation[] annotations = personClass.getAnnotations();
         printAnnotaitons(annotations);
 
         Method[] methods = personClass.getMethods();
         if (methods != null) {
             for (Method method : methods) {
-                // 打印方法的注解信息
+                // 打印 Method 上的注解信息
                 Annotation[] methodAnnotations = method.getAnnotations();
                 printAnnotaitons(methodAnnotations);
             }
@@ -35,7 +37,21 @@ public class Test {
         if (annotations != null) {
             for (Annotation annotation : annotations) {
                 System.out.println(annotation.toString());
+                if (annotation instanceof Author) {
+                    System.out.println("This is Author Annotation");
+                    Author author = (Author) annotation;
+                    System.out.println("Author is " + author.value());
+                }
+                if (annotation instanceof AnnotationDemo) {
+                    System.out.println("This is AnnotationDemo Annotation");
+                    AnnotationDemo demo = (AnnotationDemo) annotation;
+                    System.out.println("AnnotationDemo.name is " + demo.name());
+                    System.out.println("AnnotationDemo.count is " + demo.count());
+                    System.out.println("AnnotationDemo.time is " + demo.time());
+                    System.out.println("AnnotationDemo.userType is " + demo.userType());
+                }
             }
         }
+
     }
 }
