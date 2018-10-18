a283f9db5bc82bff2aa80738bb2dd31a60b9a2da
houxx <hou_xiangxiang@126.com>
2018-10-18 15:57:32 +0800

    添加 junit 测试用例

---
+++ java-annotation-test/src/test/java/com/hxx/api/ApiDemoTest.java
@@ -0,0 +1,29 @@
+package com.hxx.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.annotation.Resource;
+
+/**
+ * ApiDemoImpl Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class) //使用junit4进行测试
+@ContextConfiguration(locations = {"classpath*:applicationContext.xml"}) //加载配置文件
+public class ApiDemoTest {
+
+    @Resource(name = "apiDemo")
+    private ApiDemo apiDemo;
+
+    @Test
+    public void testWork() throws Exception {
+        apiDemo.work();
+    }
+
+
+} 
